d2ce16d8af3c63a46c5cf3351dcca860a88ab812
Md. Saiful Alam <76889781+saifulppro@users.noreply.github.com>
2023-01-09 17:25:22 +0600

    Update search-query.php

--- search-query.php
+++ search-query.php
@@ -1,5 +1,5 @@
 public function f_name(){
-        $this->leads = Model_name::where('name', 'like','%' . $this->search . '%')
+        $this->$public_variable_name(populate data from related Model) = Model_name::where('name', 'like','%' . $this->search . '%')
         ->orWhere('email', 'like','%' . $this->search . '%')
         ->orWhere('phone', 'like','%' . $this->search . '%')
         ->get();
